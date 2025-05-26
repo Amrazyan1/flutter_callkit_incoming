4d8a1ce1c9f5b1fc370904b014141e606adc5d24
Amrazyan1 <arman.amrazyan@softconstruct.com>
2025-05-26 18:10:08 +0400

    Update CallkitNotificationManager.kt

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -532,7 +532,7 @@ class CallkitNotificationManager(private val context: Context) {
     }
 
 
-    private fun getNotificationManager(): NotificationManagerCompat {
+    fun getNotificationManager(): NotificationManagerCompat {
         return NotificationManagerCompat.from(context)
     }
 
@@ -566,12 +566,16 @@ class CallkitNotificationManager(private val context: Context) {
         }
     }
 
-    fun requestFullIntentPermission(activity: Activity?) {
+    fun requestFullIntentPermission(activity: Activity?, map: Map<String, Any>) {
+        this.dataNotificationPermission = map
         val canUseFullScreenIntent = getNotificationManager().canUseFullScreenIntent();
         if (!canUseFullScreenIntent && Build.VERSION.SDK_INT > 33) {
             val intent = Intent(Settings.ACTION_MANAGE_APP_USE_FULL_SCREEN_INTENT).apply {
-                data = Uri.fromParts("package", activity?.packageName, null)
+
+                data =  Uri.fromParts("package", activity?.packageName, null)
+
             }
+
             activity?.startActivity(intent)
         }
     }
